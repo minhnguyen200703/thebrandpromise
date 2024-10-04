f4e3ede8a7cb40fa66f3910f147f1f901bd5dee7
Minh Nguyen <101308871+minhnguyen200703@users.noreply.github.com>
2024-10-04 08:58:06 +0800

    Minor update

--- assets/js/effect.js
+++ assets/js/effect.js
@@ -2,7 +2,7 @@ document.addEventListener('DOMContentLoaded', function () {
     const slideElements = document.querySelectorAll('.slide-up');
 
     const observerOptions = {
-        threshold: 0.2 // Trigger when 20% of the element is in the viewport
+        threshold: 0.1 // Trigger when 20% of the element is in the viewport
     };
 
     const observer = new IntersectionObserver((entries, observer) => {
